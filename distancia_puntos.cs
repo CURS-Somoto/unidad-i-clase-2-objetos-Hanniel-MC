1202f63ba96876b592209b8dd5b1bb6235f9c728
Hanniel-MC <147262206+Hanniel-MC@users.noreply.github.com>
2023-11-03 21:08:01 -0600

    Update distancia_puntos.cs
    entrega de trabajo

--- distancia_puntos.cs
+++ distancia_puntos.cs
@@ -1,1 +1,42 @@
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto origen = new Punto(2, 5);
+            Punto destino = new Punto(6, 9);
 
+            decimal distancia = origen.MedirDistancia(destino);
+
+            Console.WriteLine("La distancia entre los puntos es: " + distancia);
+            Console.ReadKey();
+           
+        }
+    }
+
+    class Punto
+    {
+        public decimal x;
+        public decimal y;
+        public decimal z;
+
+        public Punto(decimal x, decimal y, decimal z = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public decimal MedirDistancia(Punto otro)
+        {
+            decimal dx = this.x - otro.x;
+            decimal dy = this.y - otro.y;
+            decimal dz = this.z - otro.z;
+
+            decimal distancia = (decimal)Math.Sqrt((double)(dx * dx + dy * dy + dz * dz));
+
+            return distancia;
+            
+        }
+    }
+
+}
